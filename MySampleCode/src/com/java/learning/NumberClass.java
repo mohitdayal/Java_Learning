7f73e9fe28640f46a11395f3aa5f7bff6591ce5e
mohitdayal <mohitdayal@gmail.com>
2015-10-18 20:58:52 -0400

    New Class addition

---
+++ MySampleCode/src/com/java/learning/NumberClass.java
@@ -0,0 +1,20 @@
+package com.java.learning;
+
+public class NumberClass {
+
+	public static void main(String[] args) {
+		
+		typeConversion();
+	}
+
+	private static void typeConversion() {
+		Float f = new Float(7.04);
+		Number n = f;
+		f= new Float(3.4);
+		Integer i = n.intValue();
+		System.out.println(i);
+		System.out.println(f);
+		
+	}
+	
+}
